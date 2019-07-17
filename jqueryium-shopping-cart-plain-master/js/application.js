7605e3875a1bc3b8525a01d99c5a9ea35d5503e9
dwddavis <50349984+dwddavis@users.noreply.github.com>
2019-07-17 17:54:20 -0500

    updated JS working add and remove button.
    item total brokend.

--- jqueryium-shopping-cart-plain-master/js/application.js
+++ jqueryium-shopping-cart-plain-master/js/application.js
@@ -1,7 +1,66 @@
+var eachItemTotal = function(ele) {
+    var itemCost = parseFloat($(ele).find('.cost').text());
+    var itemQuantity = parseFloat($(ele).find('.quantity input').val());
+
+    var itemTotal = itemCost * itemQuantity;
+    $(ele).children('.itemTotal').html(itemTotal);
+
+    return itemTotal;
+  }
 
 
-$(document).ready(function(){
+var sum = function (acc, x){ return acc + x; };
+
+var updateCart = function () {
+  var cartTotalArray = [];
+
   $('tbody tr').each(function(i,ele){
-    console.log($(ele).children().first().text());
+    var itemTotal = eachItemTotal(ele);
+    cartTotalArray.push(itemTotal);
   });
-});
+
+var cartTotalCondensed = cartTotalArray.reduce(sum);
+  $('#cartTotal').html(cartTotalCondensed);
+}
+
+
+
+
+
+  $(document).ready(function(){
+    updateCart();
+
+    $('.btn.remove').on('click',function(event){
+      $(this).closest('tr').remove();
+      updateCart();
+
+      });
+
+      var timeout;
+      $(document).on('input', 'tr input',function () {
+        clearTimeout(timeout);
+        timeout = setTimeout(function () {
+          updateCart();
+        }, 1000);
+      });
+
+    $('#addItem').on('submit', function (event) {
+      event.preventDefault();
+      var item = $(this).children('[name=item]').val();
+      var cost = $(this).children('[name=cost]').val();
+
+
+      $('tbody').append('<tr>' +
+      '<td class="item">' + item + '</td>' +
+      '<td class="cost"><span>$</span>' + cost + '</td>' +
+      '<td class="quantity"><span id="qty">QTY </span><input type="number" value="1"/></td>' +
+      '<td><button class="btn btn-light btn-sm remove">remove</button></td>' +
+      '<td class="itemTotal"></td>' +
+      '</tr>');
+
+      updateCart();
+      $(this).children('[name=item]').val('');
+      $(this).children('[name=cost]').val('');
+
+    });
+  });
