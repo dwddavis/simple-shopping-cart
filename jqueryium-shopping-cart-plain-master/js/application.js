e9e81b9d2bf6f96608d479d8a40843b13da621ea
dwddavis <50349984+dwddavis@users.noreply.github.com>
2019-07-22 09:58:30 -0500

    Cleaned up JS indentions on final

--- jqueryium-shopping-cart-plain-master/js/application.js
+++ jqueryium-shopping-cart-plain-master/js/application.js
@@ -1,13 +1,15 @@
 var eachItemTotal = function(ele) {
-    var itemCost = parseFloat($(ele).find('.costpart').text());
-    var itemQuantity = parseFloat($(ele).find('.quantity input').val());
+  var itemCost = parseFloat($(ele).find('.costpart').text());
+  var itemQuantity = parseFloat($(ele).find('.quantity input').val());
+  var itemTotal = itemCost * itemQuantity;
 
-    var itemTotal = itemCost * itemQuantity;
-
-    $(ele).find('.itemTotal').html(itemTotal.toFixed(2));
-
+  $(ele).find('.itemTotal').html(itemTotal.toFixed(2));
     return itemTotal;
-  }
+}
+
+
+
+
 
 
 var sum = function (acc, x){ return acc + x; };
@@ -20,7 +22,7 @@ var updateCart = function () {
     cartTotalArray.push(itemTotal);
   });
 
-var cartTotalCondensed = cartTotalArray.reduce(sum);
+  var cartTotalCondensed = cartTotalArray.reduce(sum);
   $('#cartTotal').html(cartTotalCondensed);
 }
 
@@ -28,39 +30,42 @@ var cartTotalCondensed = cartTotalArray.reduce(sum);
 
 
 
-  $(document).ready(function(){
-    updateCart();
+
+
+
+$(document).ready(function(){
+  updateCart();
 
   $(document).on('click', '.btn.remove', function (event) {
   $(this).closest('tr').remove();
   updateCart();
   });
 
-      var timeout;
-      $(document).on('input', 'tr input',function () {
-        clearTimeout(timeout);
-        timeout = setTimeout(function () {
-          updateCart();
-        }, 1000);
-      });
-
-    $('#addItem').on('submit', function (event) {
-      event.preventDefault();
-      var item = $(this).children('[name=item]').val();
-      var cost = $(this).children('[name=cost]').val();
-      var afterCost = parseFloat(cost).toFixed(2);
-
-      $('tbody').append('<tr>' +
-      '<td class="item">' + item + '</td>' +
-      '<td class="cost"><span>$ </span><span class="costpart">' + afterCost + '</span></td>' +
-      '<td class="quantity"><span id="qty">QTY </span><input type="number" value="1"/></td>' +
-      '<td><button class="btn btn-light btn-sm remove">remove</button></td>' +
-      '<td class="itemTotalTd">$ <span class="itemTotal"></span></td>' +
-      '</tr>');
-
+  var timeout;
+  $(document).on('input', 'tr input',function () {
+    clearTimeout(timeout);
+    timeout = setTimeout(function () {
       updateCart();
-      $(this).children('[name=item]').val('');
-      $(this).children('[name=cost]').val('');
-
-    });
+    }, 1000);
   });
+
+  $('#addItem').on('submit', function (event) {
+    event.preventDefault();
+    var item = $(this).children('[name=item]').val();
+    var cost = $(this).children('[name=cost]').val();
+    var afterCost = parseFloat(cost).toFixed(2);
+
+    $('tbody').append('<tr>' +
+    '<td class="item">' + item + '</td>' +
+    '<td class="cost"><span>$ </span><span class="costpart">' + afterCost + '</span></td>' +
+    '<td class="quantity"><span id="qty">QTY </span><input type="number" value="1"/></td>' +
+    '<td><button class="btn btn-light btn-sm remove">remove</button></td>' +
+    '<td class="itemTotalTd">$ <span class="itemTotal"></span></td>' +
+    '</tr>');
+
+    updateCart();
+    $(this).children('[name=item]').val('');
+    $(this).children('[name=cost]').val('');
+
+  });
+});
